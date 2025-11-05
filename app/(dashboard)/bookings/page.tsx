6d2cba626d2a842b48b501ba826585c9b84d5b3f
Mehdi Asadli <mehdiasadli@azericard.com>
2025-11-05 09:32:07 +0400

    update: not implemented desc on bookings page

--- app/(dashboard)/bookings/page.tsx
+++ app/(dashboard)/bookings/page.tsx
@@ -5,7 +5,7 @@ export default function BookingsPage() {
   return (
     <div>
       <PageHeader title='Bookings' description='Manage your bookings' />
-      <NotImplemented />
+      <NotImplemented description='However you can still view individual bookings. For that, you need to go to either a vehicle or a user details page and click on the View Details button on booking card.' />
     </div>
   );
 }
